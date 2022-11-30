233f55e50bc2d2569d5531d83a6831f02be206a8
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-11-30 13:12:41 +0200

    Add line

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import arrow from '../../assets/icon-arrow-down.svg';
 import styles from './Question.module.scss';
 
 const Question = () => {
+  const [show, setShow] = useState(false);
   return (
-    <section>
-      <p>How many team members can I invite?</p>
+    <section onClick={() => setShow((prevState) => setShow(!prevState))}>
+      <div className={styles.question}>
+        <p>How many team members can I invite?</p>
+        <img src={arrow} alt="arrow" className={show && styles.img} />
+      </div>
+
+      {show && (
+        <p>
+          You can invite up to 2 additional users on the Free plan. There is no limit on team
+          members for the Premium plan.
+        </p>
+      )}
     </section>
   );
 };

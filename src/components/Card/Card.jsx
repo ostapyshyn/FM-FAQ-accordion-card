409fc49d715af4c5ce9f4f5b622c792fa42a81f9
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-12-01 23:43:19 +0200

    Fix desktop styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import woman from '../../assets/illustration-woman-online-desktop.svg';
 import woman_mob from '../../assets/illustration-woman-online-mobile.svg';
+import box from '../../assets/illustration-box-desktop.svg';
+import bg_desktop from '../../assets/bg-pattern-desktop.svg';
 import questions from '../../assets/questions.js';
 import Question from '../Question';
 
@@ -13,8 +15,10 @@ const Card = () => {
         <source media="(min-width: 800px)" srcSet={woman} />
         <img src={woman_mob} alt="woman" />
       </picture>
+      <img src={box} alt="box" className={styles.box} />
+      <img src={bg_desktop} alt="box" className={styles.box_bg} />
       <div className={styles.faq}>
-        <h1>FAQ</h1>
+        <h1 className={styles.title}>FAQ</h1>
         {questions.map((question) => {
           return (
             <Question key={question.id} question={question.question} answer={question.answer} />
